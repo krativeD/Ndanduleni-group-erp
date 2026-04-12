5ebdde5da4aa928235857b639b8b74431278ed2d
krativeD <kratived25@gmail.com>
2026-04-12 16:09:12 +0200

    Update AuthCard.jsx

--- src/components/auth/AuthCard.jsx
+++ src/components/auth/AuthCard.jsx
@@ -8,7 +8,7 @@ const AuthCard = ({ title, children }) => {
       <Card className={styles.authCard}>
         <div className={styles.logoWrapper}>
           <img src="/logo.png" alt="Ndanduleni group" className={styles.logo} />
-          <h1>Ndanduleni group</h1>
+          <h1>NDANDULENI GROUP</h1>
         </div>
         {title && <h2 className={styles.title}>{title}</h2>}
         {children}
