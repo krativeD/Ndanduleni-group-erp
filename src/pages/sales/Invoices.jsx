31d1eff046e317ab1038784fd26bf334e6492442
krativeD <kratived25@gmail.com>
2026-04-14 08:49:04 +0000

    Update Invoices.jsx

--- src/pages/sales/Invoices.jsx
+++ src/pages/sales/Invoices.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Layout from '../../components/common/Layout';
 import InvoiceList from '../../components/sales/InvoiceList';
 import { useInvoices } from '../../hooks/useSales';
 import Loader from '../../components/common/Loader';
@@ -7,15 +6,8 @@ import styles from './SalesStyles.module.css';
 
 const Invoices = () => {
   const { invoices, loading } = useInvoices();
-  if (loading) return <Layout><Loader /></Layout>;
-  return (
-    <Layout>
-      <div className={styles.pageContainer}>
-        <div className={styles.pageHeader}><h1>Invoices</h1></div>
-        <InvoiceList invoices={invoices} onGenerate={() => console.log('Generate invoice')} />
-      </div>
-    </Layout>
-  );
+  if (loading) return <Loader />;
+  return <InvoiceList invoices={invoices} onGenerate={() => console.log('Generate invoice')} />;
 };
 
 export default Invoices;
