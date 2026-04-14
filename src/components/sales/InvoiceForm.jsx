366243669c8d458bfb9bd0ce4223bf9445cb9893
krativeD <kratived25@gmail.com>
2026-04-14 12:47:43 +0000

    Update InvoiceForm.jsx

--- src/components/sales/InvoiceForm.jsx
+++ src/components/sales/InvoiceForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '../common/Card';
 import Input from '../common/Input';
 import Button from '../common/Button';
