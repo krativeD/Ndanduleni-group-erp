8ec104ce0739405fe2e7565e8dfc42b1d73c8ce5
krativeD <kratived25@gmail.com>
2026-04-14 08:49:21 +0000

    Update Payments.jsx

--- src/pages/sales/Payments.jsx
+++ src/pages/sales/Payments.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import Layout from '../../components/common/Layout';
 import PaymentTracker from '../../components/sales/PaymentTracker';
 import { usePayments } from '../../hooks/useSales';
 import Loader from '../../components/common/Loader';
-import styles from './SalesStyles.module.css';
 
 const Payments = () => {
   const { payments, loading } = usePayments();
-  if (loading) return <Layout><Loader /></Layout>;
-  return (
-    <Layout>
-      <div className={styles.pageContainer}>
-        <div className={styles.pageHeader}><h1>Payments</h1></div>
-        <PaymentTracker payments={payments} onRecord={() => console.log('Record payment')} />
-      </div>
-    </Layout>
-  );
+  if (loading) return <Loader />;
+  return <PaymentTracker payments={payments} onRecord={() => console.log('Record payment')} />;
 };
 
 export default Payments;
