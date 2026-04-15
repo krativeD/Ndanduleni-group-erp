513cb87b743595b5323ca97a36a3a31831d17ac2
krativeD <kratived25@gmail.com>
2026-04-15 09:39:59 +0200

    Update Payables.jsx

--- src/pages/finance/Payables.jsx
+++ src/pages/finance/Payables.jsx
@@ -4,10 +4,18 @@ import { useAccountsPayable } from '../../hooks/useFinance';
 import Loader from '../../components/common/Loader';
 
 const Payables = () => {
-  const { payables, loading, recordPayment } = useAccountsPayable();
+  const { payables, loading, addPayable, updatePayable, deletePayable, recordPayment } = useAccountsPayable();
+
+  const handleAdd = () => console.log('Add payable');
 
   if (loading) return <Loader />;
-  return <AccountsPayable payables={payables} onRecordPayment={recordPayment} />;
+  return (
+    <AccountsPayable 
+      payables={payables} 
+      onRecordPayment={recordPayment}
+      onAddPayable={handleAdd}
+    />
+  );
 };
 
 export default Payables;
