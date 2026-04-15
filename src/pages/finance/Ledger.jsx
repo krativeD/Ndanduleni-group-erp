27d29d246326e0b3a94300e8e41c819f637aa6ff
krativeD <kratived25@gmail.com>
2026-04-15 08:14:18 +0200

    Create Ledger.jsx

---
+++ src/pages/finance/Ledger.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GeneralLedger from '../../components/finance/GeneralLedger';
+import { useGeneralLedger } from '../../hooks/useFinance';
+import Loader from '../../components/common/Loader';
+
+const Ledger = () => {
+  const { ledger, loading, addEntry } = useGeneralLedger();
+
+  const handleAddEntry = () => console.log('Add journal entry');
+
+  if (loading) return <Loader />;
+  return <GeneralLedger ledger={ledger} onAddEntry={handleAddEntry} />;
+};
+
+export default Ledger;
